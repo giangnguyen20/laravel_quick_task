2f8b9cb7470aff0ae990fbd3c095472e628ca16c
giang nguyen <giynnguyen1120@gmail.com>
2022-03-21 16:38:33 +0700

    chapter1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    chapter1
                 </div>
 
                 <div class="links">
