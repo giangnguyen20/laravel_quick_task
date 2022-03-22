576ba848d68d3623325b2429e8f2d42b46c76f64
giang nguyen <giynnguyen1120@gmail.com>
2022-03-22 08:45:29 +0700

    chaper2 bai tap

---
+++ app/offices.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class offices extends Model
+{
+   /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'offices';
+
+    protected $fillable = ['id', 'user_id', 'content'];
+
+    public $timestamps = true;
+}
